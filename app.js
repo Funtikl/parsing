a6e8dc5a18c0e500e6ed6172bde788ec87d616ae
Funtikl <rustam.fuad@gmail.com>
2018-03-12 22:43:03 +0400

    From home

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // const express = require("express");
 const request = require("request");
 const cheerio = require("cheerio");
-const async = require('async');
-const fs = require("fs");
+const async = require("async");
 // const app = express();
 
 //database
@@ -18,208 +17,90 @@ connection.connect(function(err) {
   console.log("Connected");
 });
 
-// url for pars
-let url = "https://www.w-t.az";
-
-// empty arrays for pushing parsed items
-let links = [];
-let cat = [];
-
-//requset function from request pack to open url
-request(url, function(err, res, body) {
-  // if statement for catching errors and cheking connection
-  if (!err && res.statusCode === 200) {
-    //define variable for easy access to site body from cheerio
-    let $ = cheerio.load(body);
-    //select navbar-nav class from body and adding method each.
-    $(".navbar-nav").each(function(i, elem) {
-      // select li>a tag from this(class), find href attr and initialise it as variable
-      let a = $(this)
-        .find("li > a")
-        .eq(0)
-        .attr("href");
-      let b = $(this)
-        .find("li > a")
-        .eq(1)
-        .attr("href");
-      let c = $(this)
-        .find("li > a")
-        .eq(2)
-        .attr("href");
-      let d = $(this)
-        .find("li > a")
-        .eq(3)
-        .attr("href");
-      let e = $(this)
-        .find("li > a")
-        .eq(4)
-        .attr("href");
-      // push all attrs to the empty links array
-      links.push(url + a);
-      links.push(url + b);
-      links.push(url + c);
-      links.push(url + d);
-      links.push(url + e);
-    });
-    // console.log(links);
-    // regular expression ilə filter
-    function regEx(val) {
-      let myReg = /c/;
-      return val.match(myReg);
-    }
-    cat = links.filter(regEx);
-//insert to database;
-
-    //select first,second url of category(cat) and init it to vars phones'n'computers
-
-    let [phones, computers, numbers, accessories, smartwatches] = cat;
-    let names = ["phones", "computers", "numbers", "accessories", "smartwatches"];
-
-    for (let i=0; i<names.length; i++){
-      cat[i] = cat[i].split(', ');
-      let sql = 'INSERT INTO categories (name, url) VALUES(?, ?)';
-connection.query(sql, [names[i], cat[i]], function(err,  result) {
-        if (err) throw err;
-        // console.log(result);
-        // console.log("Records inserted");
-    });
-    connection.query( "DELETE n1 FROM categories n1, categories n2 WHERE n1.id > n2.id AND n1.name = n2.name");
-    }
-    request(phones, function(err, res, body) {
-      // check
-      if (!err && res.statusCode === 200) {
-        let $ = cheerio.load(body);
-               // select and filter item class
-        $(".items div").each(function() {
-          let title = $(this).find('.item_a').first().attr('title');
-          let url =  $(this).find('.item_a').attr('href');
-          let priceButton = $(this).find('.product_buttons').text();
-          // console.log(title)
-          priceButton = priceButton.split(' ');
-          // console.log(priceButton);
-          if(priceButton.length===5){
-           let price1 = 'INSERT INTO products (nisye, nagd) VALUES(?, ?)';
-           connection.query(price1, [priceButton[1],priceButton[3]],function(err,result){
-             if(err) throw err;
-            //  console.log(result);
-           });
-          }
-          else if(priceButton===3){
-            let price2 = 'INSERT INTO products (nisye, nagd) VALUES(?, ?)';
-            connection.query(price2, [priceButton[1], ''],function(err,result){
-              if(err) throw err;
-              // console.log(result);
-            }
-            )}
-          // let image = $(this).find('.item_a').attr('src');
-           
-            let sql = 'INSERT INTO products (name, url) VALUES(?, ?)';
-            if(title!==undefined){  
-          connection.query(sql, [title, "www.w-t.az"+url], function(err,  result) {
-
-            if (err) throw err;
-            console.log(result);
-            console.log("Records inserted");
-
-          });}
-         
-            connection.query("delete from products where name = '';");
-            connection.query( "DELETE n1 FROM products n1, products n2 WHERE n1.id > n2.id AND n1.name = n2.name");
-      });
-      }
-    });
-    // open computers that was a cat second url
-    request(computers, function(err, res, body) {
-      if (!err && res.statusCode === 200) {
-        let $ = cheerio.load(body);
-           $(".items div").filter(function() {
-          let title = $(this).find('.item_a').find('h2').text();
-          let url =  $(this).find('.item_a').attr('href');
-          let priceButton = $(this).find('.product_buttons').text();
-          priceButton = priceButton.split(' ');
-        //  console.log(priceButton);
-
-         if(priceButton.length===5){
-           let price1 = 'INSERT INTO products (nagd, nisye) VALUES(?, ?)';
-           connection.query(price1, [priceButton[1],priceButton[3]],function(err,result){
-             if(err) throw err;
-            //  console.log(result);
-           })
-          }
-        else if(priceButton===3){
-            let price2 = 'INSERT INTO products (nagd, nisye) VALUES(?, ?)';
-            connection.query(price2, [priceButton[1], ''],function(err,result){
-              if(err) throw err;
-              // console.log(result);
-            }
-            )}
-          // console.log(title);
-          let sql = 'INSERT INTO products (name, url) VALUES(?, ?)';
-          connection.query(sql, [title, "www.w-t.az"+url], function(err,  result) {
-
-            if (err) throw err;
-            // console.log(result);
-            // console.log("Records inserted");
-
-          });
-          });
-         }
-      connection.query("delete from products where name = '';");
-      connection.query( "DELETE n1 FROM products n1, products n2 WHERE n1.id > n2.id AND n1.name = n2.name");
-
-    request(smartwatches , function(err,res,body){
-        if(!err && res.statusCode === 200){
-          let $ = cheerio.load(body);
-          $(".items div").filter(function() {
-            let title = $(this).find('.item_a').find('h2').text();
-            let url =  $(this).find('.item_a').attr('href');
-            let priceButton = $(this).find('.product_buttons').text();
-          priceButton = priceButton.split(' ');
-        //  console.log(priceButton);
-
-         if(priceButton.length===5){
-           let price1 = 'INSERT INTO products (nagd, nisye) VALUES(?, ?)';
-           connection.query(price1, [priceButton[1],priceButton[3]],function(err,result){
-             if(err) throw err;
-            //  console.log(result);
-           })
-          }
-        else if(priceButton===3){
-            let price2 = 'INSERT INTO products (nagd, nisye) VALUES(?, ?)';
-            connection.query(price2, [priceButton[1], ''],function(err,result){
-              if(err) throw err;
-              // console.log(result);
-            }
-            )}
-            // console.log(title);
-            let sql = 'INSERT INTO products (name, url) VALUES(?, ?)';
-          connection.query(sql, [title, "www.w-t.az"+url], function(err,  result) {
-
-            if (err) throw err;
-            // console.log(result);
-            // console.log("Records inserted");
-
-          });
+const url = "https://www.w-t.az";
+let names = ["phones", "computers", "numbers", "accessories", "smartwatches"];
+let master = function(linkTo) {
+  request(linkTo, function(err, res, body) {
+    if (!err && res.statusCode === 200) {
+      let $ = cheerio.load(body);
+      $(".navbar-nav >li").each(function(i, elem) {
+        let cat = $(this)
+          .find("a")
+          .attr("href");
+        cat = url + cat;
+        let sql = "INSERT INTO categories (name, url) VALUES(?, ?)";
+        // console.log(cat)
+        connection.query(sql, [names[i], cat], function(err, result) {
+          if (err) throw err;
+          // console.log(result);
+          // console.log("Records inserted");
+          connection.query(
+            "DELETE n1 FROM categories n1, categories n2 WHERE n1.id > n2.id AND n1.name = n2.name"
+          );
         });
-          connection.query("delete from products where name = '';");
-          connection.query( "DELETE n1 FROM products n1, products n2 WHERE n1.id > n2.id AND n1.name = n2.name");
+      });
+    }
+  });
+};
+let childrens = function() {
+  connection.query("SELECT * FROM categories", function(err, row, fields) {
+    for (let i in row) {
+      // console.log(row[i].url)
+      request(row[i].url, function(err, res, body) {
+        console.log(row[i].url);
+        if (!err && res.statusCode === 200) {
+          let $ = cheerio.load(body);
+          $(".items div").each(function() {
+            let title = $(this)
+              .find(".item_a")
+              .first()
+              .attr("title");
+            let url = $(this)
+              .find(".item_a")
+              .attr("href");
+            if (title !== undefined) {
+              console.log(url);
+            }
+          });
         }
+      });
+    }
+  });
+};
+childrens();
+// console.log(master);
 
-      })
-      // request(numbers, function(err, res, body){
-      //   if(!err && res.statusCode===200){
-      //     let $ = cheerio.load(body);
-      //     $(".container").filter(function(){
-      //       let number = $(this).find('strong').text();
-      //       setTimeout(function(){console.log(number.split([' ),']))}, 3000);
-      //     });
-      //   }
-      // })
-    });
+// let master1 = function(linkTo){
+//     request(linkTo, function(err, res, body){
+//       if(!err && res.statusCode ===200){
+//         let $ = cheerio.load(body);
+//       $(".navbar-nav >li").each(function(i, elem){
+//         let cat =  $(this).find("a").attr("href");
+//         // console.log(cat);
+//         let childrens = function(linkInTo){
+//           for(let i = 0; i<linkInTo.length;i++){
+//             request(linkInTo[i],function(err, res, body){
+//               if(!err && res.statusCode === 200){
+//                 $(".items div").each(function(){
+//                   let title = $(this).find('.item_a').first().attr('title');
+//                   let innerUrl =  $(this).find('.item_a').attr('href');
+//                   let price = $(this).find('.product_buttons').find('a').text();
+//                   let image = $(this).find('.item_a').attr('src');
+//                   console.log(title);
+//                 })
+//               }
+//              })
+//           }
 
-  }
+//           }
+//         console.log(childrens(cat));
+//            })
 
-});
+//         }
+//     })
+// }
+
+master(url);
 
 // const port = 3000;
 // app.listen(port, ()=>{
